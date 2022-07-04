353279b4c5d901afa4636733d847ae9ba9410d78
malik6900 <alikarriem786@gmail.com>
2022-07-04 23:00:35 +0200

    db updates

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, 'postgres://aandvpgndmkuvy:6fdcf2aae3f681cc1b31cc2266026a904cc2c8894ecf7570fc841df39840b803@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d7up58kmq5cckm', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
