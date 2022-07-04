cb79768694c6f636c7582188ae3a9502b7a8bfbd
malik6900 <alikarriem786@gmail.com>
2022-07-04 23:28:10 +0200

    connection string

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,4 +1,4 @@
-const parse = require('pg-connection-string').parse;
+const parse = require('postgres://qpnatusujwrdvv:b2e757dff90704ce5341ffb778804fecf41748d3fdc66711c949cc889be8e9d4@ec2-44-205-41-76.compute-1.amazonaws.com:5432/dfhoe1ert2sua9').parse;
 const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
