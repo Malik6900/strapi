b3d4601752f3179e13eaaac5566fcf66cf7b38f0
malik6900 <alikarriem786@gmail.com>
2022-07-04 20:04:53 +0200

    th5 commit

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, 'postgres://rzqitpooyqwybw:35439a98c4175212b64591dd0e8663f9901a95a99790d2e327fbfd9b85f0845b@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d1625mm3nm6gin', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(__dirname, 'postgres://aandvpgndmkuvy:6fdcf2aae3f681cc1b31cc2266026a904cc2c8894ecf7570fc841df39840b803@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d7up58kmq5cckm', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
